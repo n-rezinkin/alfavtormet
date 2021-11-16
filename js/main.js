6d46face5f934061cdf379c25efb015f2ea2fc0a
n-rezinkin <89951085+n-rezinkin@users.noreply.github.com>
2021-11-16 03:12:48 +0300

    11
    11

--- js/main.js
+++ js/main.js
@@ -5,6 +5,7 @@ burgerBtn.addEventListener("click", myFunction);
 function myFunction() {
    const menu = document.getElementById('menu');
    menu.classList.toggle("active")
+   burgerBtn.classList.toggle("active")
    // const menuList = document.getElementById('menu__list');
    // menuList.classList.toggle("active")
    // const menuInfo = document.getElementById('menu__info');
